a3fc877d568b8407edb1d6c43117f5008fa93de8
AnnisaPrinayanti <32536255+AnnisaPrinayanti@users.noreply.github.com>
2017-12-10 14:17:04 +0700

    Add files via upload

---
+++ Pertemuan 9/Hewan.java
@@ -0,0 +1,8 @@
+//package oop.polymorphism;
+
+class Hewan {
+	protected void nama() {
+		System.out.println("Nama Hewan");
+	}
+}
+
---
+++ Pertemuan 9/MainHewan.java
@@ -0,0 +1,9 @@
+public class MainHewan {
+	public static void main(String[] args) {
+		Hewan ayamBoiler = new Ayam();
+		Hewan bebekAngsa = new Bebek();
+		ayamBoiler.nama();
+		bebekAngsa.nama();
+
+	}
+}
